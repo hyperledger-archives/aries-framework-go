78de17831c68db0be86821a7c33c3d67f823b307
Firas Qutishat <firas.qutishat@securekey.com>
2019-09-18 13:40:37 -0400

    Added states for introduce protocol
    Signed-off-by: Andrii Soluk <isoluchok@gmail.com>

---
+++ pkg/didcomm/protocol/introduce/states.go
@@ -0,0 +1,188 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package introduce
+
+import (
+	"errors"
+
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/dispatcher"
+)
+
+const (
+	// common states
+	stateNameNoop  = "noop"
+	stateNameStart = "start"
+	stateNameDone  = "done"
+
+	// introducer states
+	stateNameArranging  = "arranging"
+	stateNameDelivering = "delivering"
+	stateNameConfirming = "confirming"
+	stateNameAbandoning = "abandoning"
+
+	// introducee states
+	stateNameDeciding = "deciding"
+	stateNameWaiting  = "waiting"
+)
+
+// The introduce protocol's state.
+type state interface {
+	// Name of this state.
+	Name() string
+	// Whether this state allows transitioning into the next state.
+	CanTransitionTo(next state) bool
+	// Executes this state, returning a followup state to be immediately executed as well.
+	// The 'noOp' state should be returned if the state has no followup.
+	Execute(msg dispatcher.DIDCommMsg) (followup state, err error)
+}
+
+// noOp state
+type noOp struct {
+}
+
+func (s *noOp) Name() string {
+	return stateNameNoop
+}
+
+func (s *noOp) CanTransitionTo(_ state) bool {
+	return false
+}
+
+func (s *noOp) Execute(_ dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("cannot execute no-op")
+}
+
+// start state
+type start struct {
+}
+
+func (s *start) Name() string {
+	return stateNameStart
+}
+
+func (s *start) CanTransitionTo(next state) bool {
+	// Introducer can go to arranging or done state
+	// Introducee can go to deciding
+	return next.Name() == stateNameArranging || next.Name() == stateNameDone || next.Name() == stateNameDeciding
+}
+
+func (s *start) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("start execute: not implemented yet")
+}
+
+// done state
+type done struct {
+}
+
+func (s *done) Name() string {
+	return stateNameDone
+}
+
+func (s *done) CanTransitionTo(next state) bool {
+	// done is the last state there is no possibility for the next state
+	return false
+}
+
+func (s *done) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("done execute: not implemented yet")
+}
+
+// arranging state
+type arranging struct {
+}
+
+func (s *arranging) Name() string {
+	return stateNameArranging
+}
+
+func (s *arranging) CanTransitionTo(next state) bool {
+	return next.Name() == stateNameArranging || next.Name() == stateNameDone || next.Name() == stateNameAbandoning
+}
+
+func (s *arranging) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("arranging execute: not implemented yet")
+}
+
+// delivering state
+type delivering struct {
+}
+
+func (s *delivering) Name() string {
+	return stateNameDelivering
+}
+
+func (s *delivering) CanTransitionTo(next state) bool {
+	return next.Name() == stateNameConfirming || next.Name() == stateNameDone || next.Name() == stateNameAbandoning
+}
+
+func (s *delivering) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("delivering execute: not implemented yet")
+}
+
+// confirming state
+type confirming struct {
+}
+
+func (s *confirming) Name() string {
+	return stateNameConfirming
+}
+
+func (s *confirming) CanTransitionTo(next state) bool {
+	return next.Name() == stateNameDone || next.Name() == stateNameAbandoning
+}
+
+func (s *confirming) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("confirming execute: not implemented yet")
+}
+
+// abandoning state
+type abandoning struct {
+}
+
+func (s *abandoning) Name() string {
+	return stateNameAbandoning
+}
+
+func (s *abandoning) CanTransitionTo(next state) bool {
+	return next.Name() == stateNameDone
+}
+
+func (s *abandoning) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("abandoning execute: not implemented yet")
+}
+
+// deciding state
+type deciding struct {
+}
+
+func (s *deciding) Name() string {
+	return stateNameDeciding
+}
+
+func (s *deciding) CanTransitionTo(next state) bool {
+	return next.Name() == stateNameWaiting || next.Name() == stateNameDone
+}
+
+func (s *deciding) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("deciding execute: not implemented yet")
+}
+
+// waiting state
+type waiting struct {
+}
+
+func (s *waiting) Name() string {
+	return stateNameWaiting
+}
+
+func (s *waiting) CanTransitionTo(next state) bool {
+	return next.Name() == stateNameDone
+}
+
+func (s *waiting) Execute(msg dispatcher.DIDCommMsg) (state, error) {
+	return nil, errors.New("waiting execute: not implemented yet")
+}
---
+++ pkg/didcomm/protocol/introduce/states_test.go
@@ -0,0 +1,214 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package introduce
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/dispatcher"
+)
+
+func notTransition(t *testing.T, st state) {
+	var allState = [...]state{
+		&noOp{}, &start{}, &done{},
+		&arranging{}, &delivering{},
+		&confirming{}, &abandoning{},
+		&deciding{}, &waiting{},
+	}
+	for _, s := range allState {
+		require.False(t, st.CanTransitionTo(s))
+	}
+}
+
+func TestNoopState(t *testing.T) {
+	noop := &noOp{}
+	require.Equal(t, stateNameNoop, noop.Name())
+	notTransition(t, noop)
+}
+
+// noOp.Execute() returns nil, error
+func TestNoOpState_Execute(t *testing.T) {
+	followup, err := (&noOp{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// start state can transition to ...
+func TestStartState(t *testing.T) {
+	st := &start{}
+	require.Equal(t, stateNameStart, st.Name())
+
+	require.True(t, st.CanTransitionTo(&arranging{}))
+	require.True(t, st.CanTransitionTo(&deciding{}))
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&confirming{}))
+	require.False(t, st.CanTransitionTo(&abandoning{}))
+	require.False(t, st.CanTransitionTo(&waiting{}))
+}
+
+func TestStartState_Execute(t *testing.T) {
+	followup, err := (&start{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// done state can transition to ...
+func TestDoneState(t *testing.T) {
+	done := &done{}
+	require.Equal(t, stateNameDone, done.Name())
+	notTransition(t, done)
+}
+
+func TestDoneState_Execute(t *testing.T) {
+	followup, err := (&done{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// arranging state can transition to ...
+func TestArrangingState(t *testing.T) {
+	st := &arranging{}
+	require.Equal(t, stateNameArranging, st.Name())
+
+	require.True(t, st.CanTransitionTo(&arranging{}))
+	require.True(t, st.CanTransitionTo(&abandoning{}))
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&confirming{}))
+	require.False(t, st.CanTransitionTo(&deciding{}))
+	require.False(t, st.CanTransitionTo(&waiting{}))
+}
+
+func TestArrangingState_Execute(t *testing.T) {
+	followup, err := (&arranging{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// delivering state can transition to ...
+func TestDeliveringState(t *testing.T) {
+	st := &delivering{}
+	require.Equal(t, stateNameDelivering, st.Name())
+
+	require.True(t, st.CanTransitionTo(&confirming{}))
+	require.True(t, st.CanTransitionTo(&abandoning{}))
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&arranging{}))
+	require.False(t, st.CanTransitionTo(&deciding{}))
+	require.False(t, st.CanTransitionTo(&waiting{}))
+}
+
+func TestDeliveringState_Execute(t *testing.T) {
+	followup, err := (&delivering{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// confirming state can transition to ...
+func TestConfirmingState(t *testing.T) {
+	st := &confirming{}
+	require.Equal(t, stateNameConfirming, st.Name())
+
+	require.True(t, st.CanTransitionTo(&abandoning{}))
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&arranging{}))
+	require.False(t, st.CanTransitionTo(&confirming{}))
+	require.False(t, st.CanTransitionTo(&deciding{}))
+	require.False(t, st.CanTransitionTo(&waiting{}))
+}
+
+func TestConfirmingState_Execute(t *testing.T) {
+	followup, err := (&confirming{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// abandoning state can transition to ...
+func TestAbandoningState(t *testing.T) {
+	st := &abandoning{}
+	require.Equal(t, stateNameAbandoning, st.Name())
+
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&arranging{}))
+	require.False(t, st.CanTransitionTo(&confirming{}))
+	require.False(t, st.CanTransitionTo(&deciding{}))
+	require.False(t, st.CanTransitionTo(&abandoning{}))
+	require.False(t, st.CanTransitionTo(&waiting{}))
+}
+
+func TestAbandoningState_Execute(t *testing.T) {
+	followup, err := (&abandoning{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// deciding state can transition to ...
+func TestDecidingState(t *testing.T) {
+	st := &deciding{}
+	require.Equal(t, stateNameDeciding, st.Name())
+
+	require.True(t, st.CanTransitionTo(&waiting{}))
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&arranging{}))
+	require.False(t, st.CanTransitionTo(&confirming{}))
+	require.False(t, st.CanTransitionTo(&deciding{}))
+	require.False(t, st.CanTransitionTo(&abandoning{}))
+}
+
+func TestDecidingState_Execute(t *testing.T) {
+	followup, err := (&deciding{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
+
+// waiting state can transition to ...
+func TestWaitingState(t *testing.T) {
+	st := &waiting{}
+	require.Equal(t, stateNameWaiting, st.Name())
+
+	require.True(t, st.CanTransitionTo(&done{}))
+
+	require.False(t, st.CanTransitionTo(&noOp{}))
+	require.False(t, st.CanTransitionTo(&start{}))
+	require.False(t, st.CanTransitionTo(&delivering{}))
+	require.False(t, st.CanTransitionTo(&arranging{}))
+	require.False(t, st.CanTransitionTo(&confirming{}))
+	require.False(t, st.CanTransitionTo(&deciding{}))
+	require.False(t, st.CanTransitionTo(&abandoning{}))
+	require.False(t, st.CanTransitionTo(&waiting{}))
+}
+
+func TestWaitingState_Execute(t *testing.T) {
+	followup, err := (&waiting{}).Execute(dispatcher.DIDCommMsg{})
+	require.Error(t, err)
+	require.Nil(t, followup)
+}
