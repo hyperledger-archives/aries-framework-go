e2de5ab30f93ca8ce85797f7d2ea8a15653e91c4
talwinder.kaur <talwinder.kaur@securekey.com>
2019-09-23 15:49:21 -0400

    feat: Implement state action - sendExchangeAck status completed
    Removing model from Introduce package as it was duplicated by issue-301
    
    closes #293
    
    Signed-off-by: talwinder.kaur <talwinder.kaur@securekey.com>

--- pkg/internal/mock/wallet/mock_wallet.go
+++ pkg/internal/mock/wallet/mock_wallet.go
@@ -7,8 +7,6 @@ SPDX-License-Identifier: Apache-2.0
 package wallet
 
 import (
-	"github.com/btcsuite/btcutil/base58"
-
 	"github.com/hyperledger/aries-framework-go/pkg/doc/did"
 	"github.com/hyperledger/aries-framework-go/pkg/wallet"
 )
@@ -23,6 +21,8 @@ type CloseableWallet struct {
 	PackErr               error
 	UnpackValue           *wallet.Envelope
 	UnpackErr             error
+	CreateDidErr          error
+	CreateDid             *did.Doc
 }
 
 // Close previously-opened wallet, removing it if so configured.
@@ -57,21 +57,5 @@ func (m *CloseableWallet) UnpackMessage(encMessage []byte) (*wallet.Envelope, er
 
 // CreateDID returns new DID Document
 func (m *CloseableWallet) CreateDID(method string, opts ...wallet.DocOpts) (*did.Doc, error) {
-	return &did.Doc{
-		Context: []string{"https://w3id.org/did/v1"},
-		ID:      "did:example:123456789abcdefghi#inbox",
-		Service: []did.Service{{
-			ServiceEndpoint: "https://localhost:8090",
-		}},
-		PublicKey: []did.PublicKey{{
-			ID:         "did:example:123456789abcdefghi#keys-1",
-			Controller: "did:example:123456789abcdefghi",
-			Type:       "Secp256k1VerificationKey2018",
-			Value:      base58.Decode("H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV")},
-			{ID: "did:example:123456789abcdefghw#key2",
-				Controller: "did:example:123456789abcdefghw",
-				Type:       "RsaVerificationKey2018",
-				Value:      base58.Decode("H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV")},
-		},
-	}, nil
+	return m.CreateDid, m.CreateDidErr
 }
