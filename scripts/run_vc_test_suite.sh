f733cec90fe5d5944f015ebe891ce837b2bc65b6
Dima <dkinoshenko@gmail.com>
2019-09-26 16:46:12 +0300

    feat: Verifiable Credential compatibility with Verifiable Claims WG Test Suite
    closes #326
    
    Signed-off-by: Dima <dkinoshenko@gmail.com>

---
+++ scripts/run_vc_test_suite.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -e
+
+echo "Running aries-framework-go Verifiable Credential test suite..."
+
+GENERATOR_NAME=aries-framework-go-gen
+VC_TEST_SUITE=vc-test-suite
+REPORT_NAME=aries-framework-go
+DIR=$(pwd)
+GENERATOR_DIR="${DIR}/pkg/doc/verifiable/test-suite"
+SUITE_DIR="${DIR}/${VC_TEST_SUITE}/suite"
+
+# build the app to test
+cd $GENERATOR_DIR
+# rename test file in order to be able to build it
+mkdir tmp
+cp verifiable_suite_test.go tmp/vc_test_suite_app.go
+cd tmp
+go build -o "${DIR}/${VC_TEST_SUITE}/${GENERATOR_NAME}"
+cd ..
+rm -rf tmp
+cd "${DIR}/${VC_TEST_SUITE}"
+export PATH=$PATH:`pwd`
+
+# get the suite
+rm -rf ${SUITE_DIR}
+git clone --depth=1 -q https://github.com/w3c/vc-test-suite suite
+
+# build the suite
+cd ${SUITE_DIR}
+npm install
+cp "${GENERATOR_DIR}/config.json" .
+
+# run the suite
+set +e
+mocha --recursive --timeout 10000 test/vc-data-model-1.0/ -R json > "implementations/${REPORT_NAME}-report.json"
+echo "See test suite results at ${SUITE_DIR}/implementations/${REPORT_NAME}-report.json"
+
+cd $DIR
