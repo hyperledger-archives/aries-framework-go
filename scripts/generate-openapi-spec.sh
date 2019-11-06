4b4c6bff0618c6ddd626da13ff6f25e8c60f0aa8
sudesh.shetty <sudesh.shetty@securekey.com>
2019-11-06 10:29:01 -0500

    chore: aries-agentd go mod
    - aries-agentd as new module so that it can be replaced to with
    customized start
    - closes #648
    
    Signed-off-by: sudesh.shetty <sudesh.shetty@securekey.com>

--- scripts/generate-openapi-spec.sh
+++ scripts/generate-openapi-spec.sh
@@ -7,17 +7,19 @@
 set -e
 
 SPEC_LOC="${SPEC_LOC}"
-SPEC_META="${SPEC_META:-cmd/aries-agentd/main.go}"
-OUTPUT="$SPEC_LOC/openAPI.yml"
+SPEC_META="${SPEC_META:-cmd/aries-agentd}"
+OUTPUT="$PWD/$SPEC_LOC/openAPI.yml"
 IMAGE="${DOCKER_IMAGE:-quay.io/goswagger/swagger}"
 IMAGE_VERSION="${DOCKER_IMAGE_VERSION:-latest}"
 
+cd $SPEC_META
+
 # generate and validate commands
-GENERATE_COMMAND="generate spec $SPEC_META -o $OUTPUT"
+GENERATE_COMMAND="generate spec main.go -o $OUTPUT"
 VALIDATE_COMMAND="validate $OUTPUT"
 
 echo "Generating Open API spec"
-docker run --rm -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) ${IMAGE}:${IMAGE_VERSION} $GENERATE_COMMAND
+docker run --rm -v $HOME:$HOME -w $(pwd) ${IMAGE}:${IMAGE_VERSION} $GENERATE_COMMAND
 
 echo "Validating generated spec"
-docker run --rm -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) ${IMAGE}:${IMAGE_VERSION} $VALIDATE_COMMAND
+docker run --rm -v $HOME:$HOME -w $(pwd) ${IMAGE}:${IMAGE_VERSION} $VALIDATE_COMMAND
