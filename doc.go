1a1b1056e9dbfda3d9d4c82a39161fcd63e730b7
Firas Qutishat <firas.qutishat@securekey.com>
2019-11-11 10:43:28 -0500

    docs: Update root project doc
    part #739
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- doc.go
+++ doc.go
@@ -6,4 +6,24 @@ SPDX-License-Identifier: Apache-2.0
 
 // Package aries enables Go developers to build solutions based on the Hyperledger Aries project
 // (https://www.hyperledger.org/projects/aries).
+//
+// Packages for end developer usage
+//
+// pkg/framework/aries: The main package of the Aries Framework. This package enables creation of context based on
+// provider options. This context is used by the client packages listed below.
+// Reference: https://godoc.org/github.com/hyperledger/aries-framework-go/pkg/framework/aries
+//
+// pkg/client/didexchange: Provides did exchange protocol through SDK.
+// Reference: https://godoc.org/github.com/hyperledger/aries-framework-go/pkg/client/didexchange
+//
+// pkg/restapi/operation/didexchange: Provides did exchange protocol through restapi.
+// Reference: https://godoc.org/github.com/hyperledger/aries-framework-go/pkg/restapi/operation/didexchange
+//
+// Basic workflow
+//
+//      1) Instantiate a aries instance using a provider options.
+//      2) Create a context using your aries instance.
+//      3) Create a client instance using its New func, passing the context.
+//      4) Use the funcs provided by each client to create your solution!
+//      5) Call aries.Close() to release resources.
 package aries
