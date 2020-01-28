40afc8860ae859e2903e92c8ac20e25a32b340b4
Andrii Soluk <isoluchok@gmail.com>
2020-01-29 00:07:21 +0200

    fix: Introduce documentation
    Signed-off-by: Andrii Soluk <isoluchok@gmail.com>

--- pkg/client/introduce/doc.go
+++ pkg/client/introduce/doc.go
@@ -8,15 +8,15 @@ SPDX-License-Identifier: Apache-2.0
 // The protocol involves at least two participants. A maximum of three participants is currently supported.
 // When creating a client a default invitation might be provided. It simplifies the usage when
 // someone wants to do introduction with your client. The simplest way to handle incoming messages (actions) is:
-// introduce := client.New(...)
-// introduce.RegisterActionEvent(actions)
-// for {
-//	select {
-//	case event := <-actions:
-//		thID, _ := event.Message.ThreadID()
-//		event.Continue(introduce.InvitationEnvelope(thID))
-//	}
-// }
+// 	introduce := client.New(...)
+// 	introduce.RegisterActionEvent(actions)
+// 	for {
+// 	  select {
+//	    case event := <-actions:
+//	      thID, _ := event.Message.ThreadID()
+//	      event.Continue(introduce.InvitationEnvelope(thID))
+//	  }
+// 	}
 //
 // But also there is an exception, when receiving a request.
 // After receiving a request one of the following functions must be executed.
@@ -24,20 +24,20 @@ SPDX-License-Identifier: Apache-2.0
 // it should be provided by one of the introducees.
 // - HandleRequestWithInvitation - is used when you have a public invitation.
 // A bit complicated way to handle incoming messages (actions) is:
-// introduce := client.New(...)
-// introduce.RegisterActionEvent(actions)
-// for {
-//	select {
-//	case event := <-actions:
-//		if event.Message.Type() == introduce.RequestMsgType {
-//			introduce.HandleRequest(event.Message, to, recipient)
-//			OR
-//			introduce.HandleRequestWithInvitation(event.Message, inv, to)
-//		}
-//		thID, _ := event.Message.ThreadID()
-//		event.Continue(introduce.InvitationEnvelope(thID))
-//	}
-// }
+// 	introduce := client.New(...)
+// 	introduce.RegisterActionEvent(actions)
+// 	for {
+// 	  select {
+// 	    case event := <-actions:
+//	      if event.Message.Type() == introduce.RequestMsgType {
+//	        introduce.HandleRequest(event.Message, to, recipient)
+//	        OR
+//	        introduce.HandleRequestWithInvitation(event.Message, inv, to)
+//	      }
+//	      thID, _ := event.Message.ThreadID()
+//	      event.Continue(introduce.InvitationEnvelope(thID))
+// 	  }
+// 	}
 //
 // Possible use cases:
 // 1) The introducer wants to commit an introduction. To do that SendProposal or SendProposalWithInvitation
