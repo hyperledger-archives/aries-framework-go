0a2b3733bc5debe4a9bcd4853abbda7b6ee61ec9
Baha Shaaban <baha.shaaban@securekey.com>
2020-01-30 10:21:09 -0500

    feat: SecretLock API and mock
    closes #1145
    
    Signed-off-by: Baha Shaaban <baha.shaaban@securekey.com>

---
+++ pkg/secretlock/api.go
@@ -0,0 +1,17 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package secretlock
+
+// package secretlock contains secret lock services to secure keys used by the Aries agent
+// and more specifically used by the KMS service.
+
+// Service provides crypto service used internally by the KMS
+type Service interface {
+	// Encrypt req for master key in keyURI
+	Encrypt(keyURI string, req *EncryptRequest) (*EncryptResponse, error)
+	// Decrypt req for master key in keyURI
+	Decrypt(keyURI string, req *DecryptRequest) (*DecryptResponse, error)
+}
