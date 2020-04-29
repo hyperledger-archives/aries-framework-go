92b4edb6c920b47fc73281cbb8fe868c5b55c4a9
Dmitriy Kinoshenko <dkinoshenko@gmail.com>
2020-04-29 16:53:42 +0300

    feat: VC strict validation based on JSON-LD context
    Signed-off-by: Dmitriy Kinoshenko <dkinoshenko@gmail.com>
    
    #1705

--- pkg/doc/verifiable/support_test.go
+++ pkg/doc/verifiable/support_test.go
@@ -33,7 +33,10 @@ const certPrefix = "testdata/crypto"
 
 //nolint:lll
 const validCredential = `{
-  "@context": "https://www.w3.org/2018/credentials/v1",
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://www.w3.org/2018/credentials/examples/v1"
+  ],
   "id": "http://example.edu/credentials/1872",
   "type": "VerifiableCredential",
   "credentialSubject": {
