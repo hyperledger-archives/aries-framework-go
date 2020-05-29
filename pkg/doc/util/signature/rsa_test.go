fa4a07e0ce188001a66c47af132442ae2898219b
Dmitriy Kinoshenko <dkinoshenko@gmail.com>
2020-05-29 10:20:16 +0300

    refactor: use signature signers in linked data proof package
    Signed-off-by: Dmitriy Kinoshenko <dkinoshenko@gmail.com>

--- pkg/doc/util/signature/rsa_test.go
+++ pkg/doc/util/signature/rsa_test.go
@@ -41,3 +41,31 @@ func TestRS256Signer_Sign(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, signature)
 }
+
+func TestNewPS256Signer(t *testing.T) {
+	signer, err := NewPS256Signer()
+
+	require.NoError(t, err)
+	require.NotNil(t, signer)
+	require.NotNil(t, signer.privateKey)
+	require.NotNil(t, signer.PublicKey)
+}
+
+func TestGetPS256Signer(t *testing.T) {
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	signer := GetPS256Signer(privKey)
+	require.NotNil(t, signer)
+	require.Equal(t, privKey, signer.privateKey)
+	require.Equal(t, &privKey.PublicKey, signer.PublicKey)
+}
+
+func TestPS256Signer_Sign(t *testing.T) {
+	signer, err := NewPS256Signer()
+	require.NoError(t, err)
+
+	signature, err := signer.Sign([]byte("test message"))
+	require.NoError(t, err)
+	require.NotEmpty(t, signature)
+}
