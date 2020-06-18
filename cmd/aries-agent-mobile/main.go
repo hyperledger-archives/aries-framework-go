3beb1fbe8f6e5dbc90bb9ef3965c6412e31d5441
Tomisin Jenrola <tomisin.jenrola@securekey.com>
2020-06-18 11:53:37 -0400

    feat: setup for mobile support
    Signed-off-by: Tomisin Jenrola <tomisin.jenrola@securekey.com>

---
+++ cmd/aries-agent-mobile/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	_ "github.com/hyperledger/aries-framework-go/cmd/aries-agent-mobile/pkg/framework"
+)
---
+++ cmd/aries-agent-mobile/pkg/framework/framework.go
@@ -0,0 +1,11 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package framework
+
+import (
+	_ "github.com/hyperledger/aries-framework-go/pkg/framework/aries"
+)
