a71dd41c03c7ec93f4c5375f4fecd35325ad6949
Kevin Griffin <griffin.kev@gmail.com>
2020-07-02 21:22:25 -0400

    message pickup client
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

---
+++ pkg/mock/didcomm/protocol/messagepickup/mock_messagepickup.go
@@ -0,0 +1,89 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package messagepickup
+
+import (
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/common/model"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/common/service"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/protocol/messagepickup"
+)
+
+// MockMessagePickupSvc mock messagepickup service
+type MockMessagePickupSvc struct {
+	service.DIDComm
+	ProtocolName       string
+	StatusRequestErr   error
+	StatusRequestFunc  func(connectionID string) (*messagepickup.Status, error)
+	BatchPickupErr     error
+	BatchPickupFunc    func(connectionID string, size int) (int, error)
+	HandleInboundFunc  func(msg service.DIDCommMsg, myDID, theirDID string) (string, error)
+	HandleOutboundFunc func(_ service.DIDCommMsg, _, _ string) (string, error)
+	AddMessageFunc     func(message *model.Envelope, theirDID string) error
+	AcceptFunc         func(msgType string) bool
+}
+
+// Name return service name
+func (m *MockMessagePickupSvc) Name() string {
+	if m.ProtocolName != "" {
+		return m.ProtocolName
+	}
+
+	return "messagepickup"
+}
+
+// StatusRequest perform StatusRequest
+func (m *MockMessagePickupSvc) StatusRequest(connectionID string) (*messagepickup.Status, error) {
+	if m.StatusRequestErr != nil {
+		return nil, m.StatusRequestErr
+	}
+
+	if m.StatusRequestFunc != nil {
+		return m.StatusRequestFunc(connectionID)
+	}
+
+	return nil, nil
+}
+
+// BatchPickup perform BatchPickup
+func (m *MockMessagePickupSvc) BatchPickup(connectionID string, size int) (int, error) {
+	if m.BatchPickupErr != nil {
+		return 0, m.BatchPickupErr
+	}
+
+	if m.BatchPickupFunc != nil {
+		return m.BatchPickupFunc(connectionID, size)
+	}
+
+	return 0, nil
+}
+
+// HandleInbound msg
+func (m *MockMessagePickupSvc) HandleInbound(msg service.DIDCommMsg, myDID, theirDID string) (string, error) {
+	if m.HandleInboundFunc != nil {
+		return m.HandleInboundFunc(msg, myDID, theirDID)
+	}
+
+	return "", nil
+}
+
+// Accept msg checks the msg type
+func (m *MockMessagePickupSvc) Accept(msgType string) bool {
+	if m.AcceptFunc != nil {
+		return m.AcceptFunc(msgType)
+	}
+
+	return true
+}
+
+// AddMessage perform AddMessage
+func (m *MockMessagePickupSvc) AddMessage(message *model.Envelope, theirDID string) error {
+	if m.AddMessageFunc != nil {
+		return m.AddMessageFunc(message, theirDID)
+	}
+
+	return nil
+}
