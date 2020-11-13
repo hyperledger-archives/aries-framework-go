b9b754b278fd226b1ae091f67dc9dc782e18ddf2
Andrii Soluk <isoluchok@gmail.com>
2020-11-13 16:05:07 +0200

    fix: Maps custom error to storage error
    Signed-off-by: Andrii Soluk <isoluchok@gmail.com>

--- cmd/aries-agent-mobile/pkg/wrappers/storage/storage.go
+++ cmd/aries-agent-mobile/pkg/wrappers/storage/storage.go
@@ -32,6 +32,17 @@ func (p *Provider) OpenStore(name string) (storage.Store, error) {
 
 type store struct{ api.Store }
 
+// Get fetches the record based on key.
+func (s *store) Get(k string) ([]byte, error) {
+	res, err := s.Store.Get(k)
+
+	if err != nil && err.Error() == storage.ErrDataNotFound.Error() {
+		return nil, storage.ErrDataNotFound
+	}
+
+	return res, err
+}
+
 // Iterator overwrites original method to satisfy the interface.
 func (s *store) Iterator(startKey, endKey string) storage.StoreIterator {
 	return &iterator{StoreIterator: s.Store.Iterator(startKey, endKey)}
