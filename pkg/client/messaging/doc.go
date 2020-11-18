dece627416c4b7f9af2a5ce7b6bf0102b7ccdde4
sudesh.shetty <sudesh.shetty@securekey.com>
2020-11-17 19:55:20 -0500

    feat: client for messaging
    - added client package for messaging features so that any command
    controller can reuse basic messaging feature to avoid code duplication
    and re-work.
    
    - closes #2323
    
    Signed-off-by: sudesh.shetty <sudesh.shetty@securekey.com>

---
+++ pkg/client/messaging/doc.go
@@ -0,0 +1,9 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package messaging enables the agent to perform many messaging features.
+// Agent can register/unregister message services. Send/Reply to messages.
+package messaging
