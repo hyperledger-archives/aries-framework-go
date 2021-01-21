236f5aeb776b63deca22dffe78328f4eb6fd6cd1
Andrii Soluk <isoluchok@gmail.com>
2021-01-21 18:10:52 +0200

    feat: 0510: Presentation-Exchange Attachment (#2472)
    Signed-off-by: Andrii Soluk <isoluchok@gmail.com>

--- pkg/didcomm/protocol/middleware/presentproof/middlewares_test.go
+++ pkg/didcomm/protocol/middleware/presentproof/middlewares_test.go
@@ -10,15 +10,21 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 
 	"github.com/hyperledger/aries-framework-go/pkg/didcomm/common/service"
 	"github.com/hyperledger/aries-framework-go/pkg/didcomm/protocol/decorator"
 	"github.com/hyperledger/aries-framework-go/pkg/didcomm/protocol/presentproof"
 	"github.com/hyperledger/aries-framework-go/pkg/doc/did"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/presexch"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/util"
 	"github.com/hyperledger/aries-framework-go/pkg/doc/verifiable"
 	mocks "github.com/hyperledger/aries-framework-go/pkg/internal/gomocks/didcomm/protocol/middleware/presentproof"
 	mocksvdr "github.com/hyperledger/aries-framework-go/pkg/internal/gomocks/framework/aries/api/vdr"
@@ -35,6 +41,26 @@ var pubKey = did.VerificationMethod{
 }
 
 const vpJWS = "eyJhbGciOiJFZERTQSIsImtpZCI6ImtleS0xIiwidHlwIjoiSldUIn0.eyJpc3MiOiJkaWQ6ZXhhbXBsZTplYmZlYjFmNzEyZWJjNmYxYzI3NmUxMmVjMjEiLCJqdGkiOiJ1cm46dXVpZDozOTc4MzQ0Zi04NTk2LTRjM2EtYTk3OC04ZmNhYmEzOTAzYzUiLCJ2cCI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSIsImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL2V4YW1wbGVzL3YxIl0sInR5cGUiOlsiVmVyaWZpYWJsZVByZXNlbnRhdGlvbiIsIlVuaXZlcnNpdHlEZWdyZWVDcmVkZW50aWFsIl0sInZlcmlmaWFibGVDcmVkZW50aWFsIjpbeyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSIsImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL2V4YW1wbGVzL3YxIl0sImNyZWRlbnRpYWxTY2hlbWEiOltdLCJjcmVkZW50aWFsU3ViamVjdCI6eyJkZWdyZWUiOnsidHlwZSI6IkJhY2hlbG9yRGVncmVlIiwidW5pdmVyc2l0eSI6Ik1JVCJ9LCJpZCI6ImRpZDpleGFtcGxlOmViZmViMWY3MTJlYmM2ZjFjMjc2ZTEyZWMyMSIsIm5hbWUiOiJKYXlkZW4gRG9lIiwic3BvdXNlIjoiZGlkOmV4YW1wbGU6YzI3NmUxMmVjMjFlYmZlYjFmNzEyZWJjNmYxIn0sImV4cGlyYXRpb25EYXRlIjoiMjAyMC0wMS0wMVQxOToyMzoyNFoiLCJpZCI6Imh0dHA6Ly9leGFtcGxlLmVkdS9jcmVkZW50aWFscy8xODcyIiwiaXNzdWFuY2VEYXRlIjoiMjAxMC0wMS0wMVQxOToyMzoyNFoiLCJpc3N1ZXIiOnsiaWQiOiJkaWQ6ZXhhbXBsZTo3NmUxMmVjNzEyZWJjNmYxYzIyMWViZmViMWYiLCJuYW1lIjoiRXhhbXBsZSBVbml2ZXJzaXR5In0sInJlZmVyZW5jZU51bWJlciI6OC4zMjk0ODQ3ZSswNywidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIlVuaXZlcnNpdHlEZWdyZWVDcmVkZW50aWFsIl19XX19.RlO_1B-7qhQNwo2mmOFUWSa8A6hwaJrtq3q7yJDkKq4k6B-EJ-oyLNM6H_g2_nko2Yg9Im1CiROFm6nK12U_AQ" //nolint:lll
+
+// nolint: gochecknoglobals
+var (
+	strFilterType = "string"
+	arrFilterType = "array"
+
+	// schemaURI is being set in init() function.
+	schemaURI string
+)
+
+// nolint: gochecknoinits
+func init() {
+	server := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(http.StatusOK)
+		//nolint: gosec,errcheck
+		res.Write([]byte(verifiable.DefaultSchema))
+	}))
+
+	schemaURI = server.URL
+}
 
 func TestSavePresentation(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -230,5 +256,217 @@ func TestSavePresentation(t *testing.T) {
 
 		require.NoError(t, SavePresentation(provider)(next).Handle(metadata))
 		require.Equal(t, props["names"], []string{vcName})
+	})
+}
+
+func TestPresentationDefinition(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	provider := mocks.NewMockProvider(ctrl)
+	provider.EXPECT().VDRegistry().Return(nil).AnyTimes()
+
+	next := presentproof.HandlerFunc(func(metadata presentproof.Metadata) error {
+		return nil
+	})
+
+	t.Run("Ignores processing", func(t *testing.T) {
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return("state-name")
+		require.NoError(t, PresentationDefinition(provider)(next).Handle(metadata))
+	})
+
+	t.Run("Ignores processing (no presentation)", func(t *testing.T) {
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return(stateNameRequestReceived)
+		metadata.EXPECT().Presentation().Return(nil)
+		metadata.EXPECT().Message().Return(service.NewDIDCommMsgMap(presentproof.RequestPresentation{
+			Type: presentproof.RequestPresentationMsgType,
+		}))
+
+		require.Nil(t, PresentationDefinition(provider)(next).Handle(metadata))
+	})
+
+	t.Run("Message decode (error)", func(t *testing.T) {
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return(stateNameRequestReceived)
+		metadata.EXPECT().Message().Return(service.NewDIDCommMsgMap(struct {
+			Type chan struct{} `json:"@type"`
+		}{}))
+
+		require.Error(t, PresentationDefinition(provider)(next).Handle(metadata))
+	})
+
+	t.Run("No attachment", func(t *testing.T) {
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return(stateNameRequestReceived)
+		metadata.EXPECT().Presentation().Return(&presentproof.Presentation{}).AnyTimes()
+		metadata.EXPECT().Message().Return(service.NewDIDCommMsgMap(presentproof.RequestPresentation{
+			Formats: []presentproof.Format{{
+				AttachID: uuid.New().String(),
+				Format:   peDefinitionFormat,
+			}},
+			Type: presentproof.RequestPresentationMsgType,
+		}))
+
+		const errMsg = "get attachment by format: not found"
+		require.EqualError(t, PresentationDefinition(provider)(next).Handle(metadata), errMsg)
+	})
+
+	t.Run("Attachment marshal (error)", func(t *testing.T) {
+		ID := uuid.New().String()
+
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return(stateNameRequestReceived)
+		metadata.EXPECT().Presentation().Return(&presentproof.Presentation{}).AnyTimes()
+		metadata.EXPECT().Message().Return(service.NewDIDCommMsgMap(presentproof.RequestPresentation{
+			Formats: []presentproof.Format{{
+				AttachID: ID,
+				Format:   peDefinitionFormat,
+			}},
+			Type: presentproof.RequestPresentationMsgType,
+			RequestPresentationsAttach: []decorator.Attachment{{
+				ID: ID,
+				Data: decorator.AttachmentData{
+					Base64: "ew==",
+				},
+			}},
+		}))
+
+		const errMsg = "unmarshal definition: unexpected end of JSON input"
+		require.EqualError(t, PresentationDefinition(provider)(next).Handle(metadata), errMsg)
+	})
+
+	t.Run("No credentials", func(t *testing.T) {
+		ID := uuid.New().String()
+
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return(stateNameRequestReceived)
+		metadata.EXPECT().Presentation().Return(&presentproof.Presentation{
+			PresentationsAttach: []decorator.Attachment{{
+				MimeType: mimeTypeApplicationLdJSON,
+				Data: decorator.AttachmentData{
+					JSON: &verifiable.Credential{
+						ID: uuid.New().String(),
+						Schemas: []verifiable.TypedID{{
+							ID:   schemaURI,
+							Type: verifiableCredentialType,
+						}},
+						CustomFields: map[string]interface{}{
+							"first_name": "Jesse",
+						},
+					},
+				},
+			}},
+		}).AnyTimes()
+		metadata.EXPECT().Message().Return(service.NewDIDCommMsgMap(presentproof.RequestPresentation{
+			Formats: []presentproof.Format{{
+				AttachID: ID,
+				Format:   peDefinitionFormat,
+			}},
+			Type: presentproof.RequestPresentationMsgType,
+			RequestPresentationsAttach: []decorator.Attachment{{
+				ID: ID,
+				Data: decorator.AttachmentData{
+					JSON: map[string]interface{}{
+						"presentation_definition": &presexch.PresentationDefinition{
+							ID: uuid.New().String(),
+							InputDescriptors: []*presexch.InputDescriptor{{
+								Schema: []*presexch.Schema{{
+									URI: schemaURI,
+								}},
+								ID: uuid.New().String(),
+								Constraints: &presexch.Constraints{
+									Fields: []*presexch.Field{{
+										Path:   []string{"$.credentialSubject.givenName", "$.credentialSubject.familyName"},
+										Filter: &presexch.Filter{Type: &strFilterType},
+									}, {
+										Path:   []string{"$.credentialSubject.type"},
+										Filter: &presexch.Filter{Type: &arrFilterType},
+									}},
+								},
+							}},
+						},
+					},
+				},
+			}},
+		}))
+
+		const errMsg = "create VP: credentials do not satisfy requirements"
+		require.EqualError(t, PresentationDefinition(provider)(next).Handle(metadata), errMsg)
+	})
+
+	t.Run("No credentials", func(t *testing.T) {
+		ID := uuid.New().String()
+
+		metadata := mocks.NewMockMetadata(ctrl)
+		metadata.EXPECT().StateName().Return(stateNameRequestReceived)
+		metadata.EXPECT().Presentation().Return(&presentproof.Presentation{
+			PresentationsAttach: []decorator.Attachment{{
+				MimeType: mimeTypeApplicationLdJSON,
+				Data: decorator.AttachmentData{
+					JSON: &verifiable.Credential{
+						ID:      "http://example.edu/credentials/1872",
+						Context: []string{"https://www.w3.org/2018/credentials/v1"},
+						Types:   []string{verifiableCredentialType},
+						Schemas: []verifiable.TypedID{{
+							ID:   schemaURI,
+							Type: "JsonSchemaValidator2018",
+						}},
+						Subject: "did:example:76e12ec712ebc6f1c221ebfeb1f",
+						Issued: &util.TimeWithTrailingZeroMsec{
+							Time: time.Now(),
+						},
+						Issuer: verifiable.Issuer{
+							ID: "did:example:76e12ec712ebc6f1c221ebfeb1f",
+						},
+						CustomFields: map[string]interface{}{
+							"first_name": "First name",
+							"last_name":  "Last name",
+							"info":       "Info",
+						},
+					},
+				},
+			}, {
+				MimeType: "application/json",
+				Data: decorator.AttachmentData{
+					JSON: map[string]struct{}{},
+				},
+			}},
+		}).AnyTimes()
+		metadata.EXPECT().Message().Return(service.NewDIDCommMsgMap(presentproof.RequestPresentation{
+			Formats: []presentproof.Format{{
+				AttachID: ID,
+				Format:   peDefinitionFormat,
+			}},
+			Type: presentproof.RequestPresentationMsgType,
+			RequestPresentationsAttach: []decorator.Attachment{{
+				ID: ID,
+				Data: decorator.AttachmentData{
+					JSON: map[string]interface{}{
+						"presentation_definition": &presexch.PresentationDefinition{
+							ID: uuid.New().String(),
+							InputDescriptors: []*presexch.InputDescriptor{{
+								ID: uuid.New().String(),
+								Schema: []*presexch.Schema{{
+									URI: schemaURI,
+								}},
+								Constraints: &presexch.Constraints{
+									Fields: []*presexch.Field{{
+										Path:   []string{"$.first_name"},
+										Filter: &presexch.Filter{Type: &strFilterType},
+									}, {
+										Path:   []string{"$.last_name"},
+										Filter: &presexch.Filter{Type: &strFilterType},
+									}},
+								},
+							}},
+						},
+					},
+				},
+			}},
+		}))
+
+		require.Nil(t, PresentationDefinition(provider)(next).Handle(metadata))
 	})
 }
