7c828070310e9b82ccb0badb5e5bee573effa4b2
Troy Ronda <troy@troyronda.com>
2021-02-09 12:31:44 -0500

    ci: restore version_var.sh (#2525)
    Signed-off-by: Troy Ronda <troy@troyronda.com>

---
+++ .github/workflows/version_var.sh
@@ -0,0 +1,23 @@
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Release Parameters
+BASE_VERSION=0.1.6
+IS_RELEASE=false
+
+ARCH=$(go env GOARCH)
+
+if [ "${IS_RELEASE}" = false ]
+then
+  EXTRA_VERSION=snapshot-$(git rev-parse --short=7 HEAD)
+  PROJECT_VERSION=$BASE_VERSION-$EXTRA_VERSION
+else
+  PROJECT_VERSION=$BASE_VERSION
+fi
+
+export IS_RELEASE
+export AGENT_IMAGE_TAG=$ARCH-$PROJECT_VERSION
+export NPM_PKG_TAG=$PROJECT_VERSION
