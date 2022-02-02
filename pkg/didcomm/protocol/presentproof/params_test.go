f2adbb3c779ed7a10b8a91d0cb676fda1eade24d
Filip Burlacu <filip.burlacu@securekey.com>
2022-02-02 15:57:22 -0500

    fix: issue-credential params support unmarshaling from DIDCommMsg
    Also refactors present-proof params for consistency.
    
    Signed-off-by: Filip Burlacu <filip.burlacu@securekey.com>

--- pkg/didcomm/protocol/presentproof/params_test.go
+++ pkg/didcomm/protocol/presentproof/params_test.go
@@ -15,9 +15,150 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/hyperledger/aries-framework-go/pkg/didcomm/common/service"
+	"github.com/hyperledger/aries-framework-go/pkg/didcomm/protocol/decorator"
 )
 
 const foo = "foo"
+
+func formatList() []Format {
+	return []Format{
+		{
+			AttachID: "attach-1",
+			Format:   "foo",
+		},
+	}
+}
+
+func attachV1List() []decorator.Attachment {
+	return []decorator.Attachment{
+		{
+			ID:   "attach-1",
+			Data: decorator.AttachmentData{},
+		},
+	}
+}
+
+func attachV2List() []decorator.AttachmentV2 {
+	return []decorator.AttachmentV2{
+		{
+			ID:   "attachv2-1",
+			Data: decorator.AttachmentData{},
+		},
+	}
+}
+
+func TestProposePresentationParams(t *testing.T) {
+	t.Run("from/to v2", func(t *testing.T) {
+		src := &ProposePresentationV2{
+			Type:            ProposePresentationMsgTypeV2,
+			Comment:         foo,
+			Formats:         formatList(),
+			ProposalsAttach: attachV1List(),
+		}
+
+		param := ProposePresentationParams{}
+
+		param.FromV2(src)
+
+		dest := param.AsV2()
+
+		require.Equal(t, src, dest)
+	})
+
+	t.Run("from/to v3", func(t *testing.T) {
+		src := &ProposePresentationV3{
+			Type: ProposePresentationMsgTypeV3,
+			Body: ProposePresentationV3Body{
+				Comment: foo,
+			},
+			Attachments: attachV2List(),
+		}
+
+		param := ProposePresentationParams{}
+
+		param.FromV3(src)
+
+		dest := param.AsV3()
+
+		require.Equal(t, src, dest)
+	})
+}
+
+func TestRequestPresentationParams(t *testing.T) {
+	t.Run("from/to v2", func(t *testing.T) {
+		src := &RequestPresentationV2{
+			Type:                       RequestPresentationMsgTypeV2,
+			Comment:                    foo,
+			WillConfirm:                true,
+			Formats:                    formatList(),
+			RequestPresentationsAttach: attachV1List(),
+		}
+
+		param := RequestPresentationParams{}
+
+		param.FromV2(src)
+
+		dest := param.AsV2()
+
+		require.Equal(t, src, dest)
+	})
+
+	t.Run("from/to v3", func(t *testing.T) {
+		src := &RequestPresentationV3{
+			Type: RequestPresentationMsgTypeV3,
+			Body: RequestPresentationV3Body{
+				Comment:     foo,
+				WillConfirm: true,
+			},
+			Attachments: attachV2List(),
+		}
+
+		param := RequestPresentationParams{}
+
+		param.FromV3(src)
+
+		dest := param.AsV3()
+
+		require.Equal(t, src, dest)
+	})
+}
+
+func TestPresentationParams(t *testing.T) {
+	t.Run("from/to v2", func(t *testing.T) {
+		src := &PresentationV2{
+			Type:                PresentationMsgTypeV2,
+			Comment:             foo,
+			Formats:             formatList(),
+			PresentationsAttach: attachV1List(),
+		}
+
+		param := PresentationParams{}
+
+		param.FromV2(src)
+
+		dest := param.AsV2()
+
+		require.Equal(t, src, dest)
+	})
+
+	t.Run("from/to v3", func(t *testing.T) {
+		src := &PresentationV3{
+			Type: PresentationMsgTypeV3,
+			Body: PresentationV3Body{
+				Comment: foo,
+			},
+			Attachments: attachV2List(),
+		}
+
+		param := PresentationParams{}
+
+		param.FromV3(src)
+
+		dest := param.AsV3()
+
+		require.Equal(t, src, dest)
+	})
+}
 
 func TestProposePresentationParams_UnmarshalJSON(t *testing.T) {
 	tests := testTable(
@@ -144,7 +285,7 @@ func testTable(expectedEmpty, expectedWithComment interface{}) []testCase {
 		},
 		{
 			name:     "comment v3",
-			srcBytes: []byte(`{"body":{"comment":"foo"}}`),
+			srcBytes: []byte(`{"id":"foo","type":"foo","body":{"comment":"foo"}}`),
 			expect:   expectedWithComment,
 		},
 	}
