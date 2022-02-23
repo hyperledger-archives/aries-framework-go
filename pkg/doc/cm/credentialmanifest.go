9063febd8e51501f5131ee75bcdfb642419cab34
sudesh.shetty <sudesh.shetty@securekey.com>
2022-02-23 17:54:08 -0500

    feat: credential manifest - include style in resolved results
    Signed-off-by: sudesh.shetty <sudesh.shetty@securekey.com>

--- pkg/doc/cm/credentialmanifest.go
+++ pkg/doc/cm/credentialmanifest.go
@@ -126,6 +126,7 @@ type ResolvedDescriptor struct {
 	Title        string              `json:"title,omitempty"`
 	Subtitle     string              `json:"subtitle,omitempty"`
 	Description  string              `json:"description,omitempty"`
+	Styles       Styles              `json:"styles,omitempty"`
 	Properties   []*ResolvedProperty `json:"properties,omitempty"`
 }
 
@@ -363,6 +364,7 @@ func resolveOutputDescriptor(outputDescriptor *OutputDescriptor,
 	resolved.Title = staticDisplayMappings.title
 	resolved.Subtitle = staticDisplayMappings.subtitle
 	resolved.Description = staticDisplayMappings.description
+	resolved.Styles = outputDescriptor.Styles
 
 	resolved.Properties, err =
 		resolveDescriptorProperties(outputDescriptor.Display.Properties, vc)
