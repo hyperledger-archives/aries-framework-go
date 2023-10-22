0313f46f5f216870aac54f21a722f6b71edd85bc
NhoxxKienn <huy.tranminh2804@gmail.com>
2023-10-22 20:06:53 +0200

    add comments for precomputation generator interface
    Signed-off-by: NhoxxKienn <huy.tranminh2804@gmail.com>

--- pkg/client/vcwallet/thresholdwallet/precomputation_generator.go
+++ pkg/client/vcwallet/thresholdwallet/precomputation_generator.go
@@ -3,6 +3,11 @@ package thresholdwallet
 import "hash"
 
 type PrecomputationsGenerator interface {
+	// GeneratePrecomputation generate the precomputations for each party
+	// and returns the public key and precomputations for each party as a Document.
 	GeneratePrecomputation(h func() hash.Hash, seed []byte, t, n, k int) (*Document, []*Document, error)
+	
+	// NextMsgIndex returns the next to be used presignature index.
+	// Returns an error if there are no presignatures left.
 	NextMsgIndex() (int, error)
 }
